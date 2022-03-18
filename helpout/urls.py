3fd2806a47c03fcf47e8489e1e8700b8fb3db951
Gabriel Sousa <gabrielsousa9@hotmail.com>
2022-03-18 11:59:23 -0300

    Main menu implementation

--- helpout/urls.py
+++ helpout/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from volah import views
+from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='main_menu'),
+    path('volah/', include('volah.urls')),
     path('admin/', admin.site.urls),
 ]
